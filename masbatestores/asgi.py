7766977cf388f6707e4abea3bb1a13b0c19b9039
MakieTech <121760681+astigPree@users.noreply.github.com>
2025-06-29 11:09:23 +0800

    paremove jarell san conflic

--- masbatestores/asgi.py
+++ masbatestores/asgi.py
@@ -3,12 +3,8 @@ ASGI config for masbatestores project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
-For more information on this file, see
-<<<<<<< HEAD
-https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
-=======
-https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
->>>>>>> 792e715d36cc695a23c91382c01e9397c1352c5a
+For more information on this file, see 
+https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/  
 """
 
 import os
