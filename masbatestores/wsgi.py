961532b049dbc94db3373571c277e66be3e290cc
VionSyrion <diabolicguy32@gmail.com>
2025-06-29 18:56:31 +0800

    Clean

--- masbatestores/wsgi.py
+++ masbatestores/wsgi.py
@@ -4,11 +4,7 @@ WSGI config for masbatestores project.
 It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-<<<<<<< HEAD
 https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
-=======
-https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
->>>>>>> 792e715d36cc695a23c91382c01e9397c1352c5a
 """
 
 import os
