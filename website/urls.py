e69ec7667aa30795c283b8edfe4a79dd299303cd
aprhyvs <173691141+aprhyvs@users.noreply.github.com>
2025-07-11 01:54:11 +0800

    add merchant-login
    svgs are to be fixed next commit

--- website/urls.py
+++ website/urls.py
@@ -4,4 +4,5 @@ from website.views import *
 
 urlpatterns = [
   path('', view=home_page, name='homepage'),
+  path('merchant-login/', view=merchant_login, name='merchantlogin')
 ]
--- website/views.py
+++ website/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def home_page(request):
     return render(request, "home/index.html")
+
+def merchant_login(request):
+    return render(request, "merchant-login/index.html")
